f58f3a65f6ef4b1e534b89c34ba47b6234c7c175
Samita Bajpai <samita.bajpai@hcl.com>
2021-12-11 00:56:53 +0530

    added test cases

--- src/components/avon-footer/test/avon-footer.spec.tsx
+++ src/components/avon-footer/test/avon-footer.spec.tsx
@@ -2,19 +2,57 @@ import { newSpecPage } from '@stencil/core/testing';
 import { AvonFooter } from '../avon-footer';
 
 describe('avon-footer', () => {
-  it('renders empty', async () => {
+  it('renders', async () => {
     const page = await newSpecPage({
       components: [AvonFooter],
-      html: `<avon-footer content=""></avon-footer>`,
+      html: `<avon-footer content="%7B%7D"></avon-footer>`,
     });
     expect(page.root).toEqualHtml(`
-      <avon-footer>
-       <mock:shadow-root>
-        
-       </mock:shadow-root>
-     </avon-footer>
+      <avon-footer content="%7B%7D">
+        <mock:shadow-root>
+         <footer class="dark site-footer">
+         <div class="footer-title">
+         <div class="footer-logo-section">
+           <span class="footer-logo"></span>
+         </div>
+         <hr class="footer-logo-line" />
+       </div>
+       <div class="footer-list-item-section">
+               <div class="footer-list-container">
+                  <div class="col footer-list-items">
+                   <div class="footer-list-title"></div>
+                   <ul class="footer-list-item"></ul>
+                 </div>
+                 <div class="col footer-list-items">
+                   <div class="footer-list-title"></div>
+                   <ul class="footer-list-item"></ul>
+                 </div>
+                 <div class="col footer-list-items">
+                   <div class="footer-list-title"></div>
+                   <ul class="footer-list-item"></ul>
+                 </div>
+                 <div class="col footer-list-items">
+                   <div class="footer-list-title"></div>
+                   <ul class="footer-list-item"></ul>
+                 </div>
+               </div>
+             </div>
+             <div class="footer-social-media-section">
+               <span class="footer-social-media-icons"></span>
+               <hr class="footer-social-media-line">
+               <span class="footer-payment-icons"></span>
+             </div>
+             <div class="footer-copyright">
+               <span class="footer-copyright-text">
+                 <a class="footer-copyright-link" href="#"></a>
+               </span>
+               <span class="footer-sitemap">
+                 <a class="footer-sitemap-link" href="#"></a>
+               </span>
+             </div>
+         </footer>
+        </mock:shadow-root>
+      </avon-footer>
     `);
   });
-
-  
-});
+ });
